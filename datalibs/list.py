21efe28de606a747fd964f161e6ee6500120fbbf
gabrielevi10 <gabrielevigomes@gmail.com>
2018-03-16 00:27:46 -0300

    Add new methods to list and some tests

--- datalibs/list.py
+++ datalibs/list.py
@@ -8,19 +8,48 @@ class List:
 		self.head = head
 		self.size = size
 
-	def insert(self, value):
-		new_node = List_node(value, None)
+	def insert(self, value, position = 0):
 		if(self.head == None):
-			self.head = new_node
-		else:
-			current_node = head
-			while(current_node.next != None):
+			self.head = List_node(value, None)
+			self.last = List_node(value, None)
+		elif(position != 0):
+			current_node = self.head
+			current_index = 1
+			while(current_index != position and current_node.next != None):
 				current_node = current_node.next
-			current_node.next = new_node
-		size = size + 1
+				current_index = current_index + 1
+			current_node.next = List_node(value, current_node.next)
+			self.size = self.size + 1
+		
 
-	def print_list(self)
+	def remove(self, position):
+		if(position != None  and  self.head != None  and  self.size != 0):	
+			if(position == "head"):
+				current_node = self.head.next
+				self.head = current_node
+			if(position == "last"):
+				current_node = self.head
+				while(current_node.next != self.last):
+					current_node = current_node.next
+				last = current_node
+		self.size = self.size - 1
+
+	def search(self, value):
+		if(self.head != None  and  self.size != 0):
+			current_node = self.head
+			flag = False
+			position = 0
+			while(current_node != None and not flag):
+				if(current_node.value == value):
+					flag = True
+					break
+				current_node = current_node.next
+				position = position + 1
+			return position
+
+	def print_list(self):
 		current_node = self.head
-		while(current_node.next != None)
-			print(value)
+		while(current_node != None):
+			print(current_node.value)
+			current_node = current_node.next
 
---
+++ testing/list_test.py
@@ -0,0 +1,35 @@
+import unittest, sys
+sys.path.append('../datalibs')
+from list import List
+
+class ListTestCase(unittest.TestCase):
+	def test_insert_head(self):
+		test_list = List(None, 0)
+		test_list.insert(0)
+		self.assertEqual(test_list.head.value, 0)
+
+	def test_insert(self):
+		test_list = List(None, 0)
+		test_list.insert(0, 0)
+		test_list.insert(1, 1)
+		test_list.insert(2, 2)
+		test_list.insert(3, 3)
+		current_node = test_list.head
+		index_test = 0
+		while(current_node != None):
+			self.assertEqual(current_node.value, index_test)
+			index_test = index_test + 1
+			current_node = current_node.next
+
+	def test_insert_any_and_search(self):
+		test_list = List(None, 0)
+		test_list.insert(0, 0)
+		test_list.insert(1, 1)
+		test_list.insert(2, 2)
+		test_list.insert(3, 3)
+		test_list.insert(4, 2)
+		self.assertEqual(test_list.search(4), 2)
+	
+
+if __name__ == '__main__':
+	unittest.main()
